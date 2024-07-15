a1108c824ddc297df7f8cc392bfb231073f7ef83
ToaDani <toadanesz@gmail.com>
2024-07-15 19:04:41 +0200

    funkciok, kulcs feltetel

--- rejtjelzes/main.py
+++ rejtjelzes/main.py
@@ -30,25 +30,29 @@ def getkeyfromvalue(_value):
     key = list(filter(lambda x : my_dict[x] == _value, my_dict))[0]
     return key
 
-def rejtjelzes(_uzenet,_kulcs):
-    _rejtjelzett = ""
-    mostani_betu = 0
-    for i in range(len(_uzenet)):
-        mostani_betu = my_dict.get(_uzenet[i]) + my_dict.get(_kulcs[i])
-        if mostani_betu > 26:
-            mostani_betu = mostani_betu % 27
-        _rejtjelzett += getkeyfromvalue(mostani_betu)
-    return _rejtjelzett
+def rejtjelzes(_uzenet : str,_kulcs : str) -> str:
+    if len(_kulcs) < len(_uzenet):
+        return None
+    else:
+        _rejtjelzett = ""
+        mostani_betu = 0
+        for i in range(len(_uzenet)):
+            mostani_betu = my_dict.get(_uzenet[i]) + my_dict.get(_kulcs[i])
+            if mostani_betu > 26:
+                mostani_betu = mostani_betu % 27
+            _rejtjelzett += getkeyfromvalue(mostani_betu)
+        return _rejtjelzett
 
-def rejtjelzesreverse(_rejtjelzett,_kulcs):
-    _uzenet = ""
-    mostani_betu = 0
-    for i in range(len(_rejtjelzett)):
-        mostani_betu = my_dict.get(_rejtjelzett[i]) - my_dict.get(_kulcs[i])
-        if mostani_betu < 0:
-            mostani_betu += 27
-        _uzenet += getkeyfromvalue(mostani_betu)
-    return _uzenet
+def rejtjelzesreverse(_rejtjelzett : str,_kulcs : str) -> str:
+    if  len(_kulcs) <  len(_rejtjelzett):
+        return None
+    else:
+        _uzenet = ""
+        mostani_betu = 0
+        for i in range(len(_rejtjelzett)):
+            mostani_betu = my_dict.get(_rejtjelzett[i]) - my_dict.get(_kulcs[i])
+            if mostani_betu < 0:
+                mostani_betu += 27
+            _uzenet += getkeyfromvalue(mostani_betu)
+        return _uzenet
 
-print(rejtjelzes("zzz","xxx"))
-print(rejtjelzesreverse("vvv","xxx"))
